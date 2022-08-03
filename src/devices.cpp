be1886d33cc2d097eb91b6aa6cf66d4e5bd3c333
Thurston Yates <thurstonalexyates@gmail.com>
2022-08-03 17:55:27 -0400

    Add more internal documentation

--- src/devices.cpp
+++ src/devices.cpp
@@ -2,13 +2,16 @@
 
 // ================================ Devices ================================ //
 
+// Inputs
 okapi::Controller controller(okapi::ControllerId::master);
 
+// Motors
 okapi::Motor drive_fl(1);
 okapi::Motor drive_fr(-2);
 okapi::Motor drive_rl(3);
 okapi::Motor drive_rr(-4);
 
+// Chassis
 std::shared_ptr<okapi::OdomChassisController> chassis =
     okapi::ChassisControllerBuilder()
         .withMotors(drive_fl, drive_fr, drive_rr, drive_rl)
