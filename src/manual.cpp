ab40cfd007301c5407162ebaf1d181794a12164d
Thurston Yates <thurstonalexyates@gmail.com>
2022-08-04 22:32:44 -0400

    Rename variables and re-map controls
    - Make the right x axis do turning and the left x axis do strafing
    - Change names of variables to make more sense

--- src/manual.cpp
+++ src/manual.cpp
@@ -7,18 +7,18 @@ void drive_control() {
 	bool active = false;
 	while (true) {
 		// Get inputs from controller
-		float rotation = controller.getAnalog(okapi::ControllerAnalog::leftX);
-		float drive = controller.getAnalog(okapi::ControllerAnalog::leftY);
 		float turn = controller.getAnalog(okapi::ControllerAnalog::rightX);
+		float drive = controller.getAnalog(okapi::ControllerAnalog::leftY);
+		float strafe = controller.getAnalog(okapi::ControllerAnalog::leftX);
 
 		// Check against deadzone
-		if (-0.05 < rotation > 0.05 || -0.05 < drive > 0.05 ||
-		    -0.05 < turn > 0.05) {
+		if (-0.05 < turn > 0.05 || -0.05 < drive > 0.05 ||
+		    -0.05 < strafe > 0.05) {
 			// Move motors
-			drive_fl.moveVelocity((drive + turn + rotation) * 200);
-			drive_fr.moveVelocity((-drive + turn + rotation) * 200);
-			drive_rl.moveVelocity((drive - turn + rotation) * 200);
-			drive_rr.moveVelocity((-drive - turn + rotation) * 200);
+			drive_fl.moveVelocity((drive + strafe + turn) * 200);
+			drive_fr.moveVelocity((-drive + strafe + turn) * 200);
+			drive_rl.moveVelocity((drive - strafe + turn) * 200);
+			drive_rr.moveVelocity((-drive - strafe + turn) * 200);
 
 			active = true;
 		} else if (active == true) { // If in deadzone check if active
