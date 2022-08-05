abd1af5a014e9a3cc8746f608479a2a47296260e
Thurston Yates <thurstonalexyates@gmail.com>
2022-08-04 22:35:23 -0400

    Temporarily change polarity of motors
    Change polarity of drive motors to work on test robot.
    (Subject to change in the future depending on the final design)

--- src/devices.cpp
+++ src/devices.cpp
@@ -7,9 +7,9 @@ okapi::Controller controller(okapi::ControllerId::master);
 
 // Motors
 okapi::Motor drive_fl(1);
-okapi::Motor drive_fr(-2);
+okapi::Motor drive_fr(2);
 okapi::Motor drive_rl(3);
-okapi::Motor drive_rr(-4);
+okapi::Motor drive_rr(4);
 
 // Chassis
 std::shared_ptr<okapi::OdomChassisController> chassis =
