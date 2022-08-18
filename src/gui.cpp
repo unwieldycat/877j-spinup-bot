37ca9c5d4baafd566cb45b1ea51b46dfea373468
Thurston Yates <thurstonalexyates@gmail.com>
2022-08-18 17:58:47 -0400

    Start work on selection screen
    This is the foundation of the selection screen, it will need tweaking
    and other features will be added.

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,5 +1,7 @@
 #include "gui.hpp"
 #include "devices.hpp"
+#include "display/lv_core/lv_obj.h"
+#include "pros/misc.hpp"
 
 // ====================== Filesystem Driver Functions ====================== //
 
@@ -37,8 +39,7 @@ static lv_fs_res_t fs_close(void *file_p) {
 	return LV_FS_RES_OK;
 }
 
-static lv_fs_res_t
-fs_read(void *file_p, void *buf, uint32_t btr, uint32_t *br) {
+static lv_fs_res_t fs_read(void *file_p, void *buf, uint32_t btr, uint32_t *br) {
 	FILE **fp = (FILE **)file_p;
 	*br = fread(buf, 1, btr, *fp);
 	return LV_FS_RES_OK;
@@ -58,11 +59,72 @@ static lv_fs_res_t fs_tell(void *file_p, uint32_t *pos_p) {
 
 // =========================== Auton Selector UI =========================== //
 
+int selected_r_id;
+std::map<int, int> btn_routine_ids;
+
+// Select action for routine buttons
+lv_res_t r_select_act(lv_obj_t *obj) {
+	int id = lv_obj_get_free_num(obj);
+	selected_r_id = btn_routine_ids.at(id);
+	return LV_RES_OK;
+}
+
+bool user_is_done;
+
+// Action for done button
+lv_res_t done_act() {
+	user_is_done = true;
+	return LV_RES_OK;
+}
+
 auton::Routine gui::selection() {
+	// Save pointer to main screen and create new screen for selection
+	lv_obj_t *main_scr = lv_scr_act();
 	lv_obj_t *select_scr = lv_page_create(NULL, NULL);
 	lv_scr_load(select_scr);
 
-	// TODO: Make selection UI
+	// Title
+	lv_obj_t *title = lv_label_create(lv_scr_act(), NULL);
+	lv_label_set_text(title, "Select autonomous routine");
+	lv_obj_align(title, NULL, LV_ALIGN_IN_TOP_MID, 0, 16);
+
+	// Routines list
+	lv_obj_t *r_list = lv_list_create(lv_scr_act(), NULL);
+	lv_obj_align(r_list, NULL, LV_ALIGN_IN_LEFT_MID, 0, 0);
+
+	// Add routines to list
+	for (auton::Routine r : auton::routines) {
+		// Store index in vector
+		static int r_index = 0;
+
+		// Get lvgl button id
+		lv_obj_t *new_btn = lv_list_add(r_list, NULL, r.description.c_str(), r_select_act);
+		int btn_id = lv_obj_get_free_num(new_btn);
+
+		// Log id and index pair and iterate index for next loop
+		btn_routine_ids.emplace(btn_id, r_index);
+		r_index++;
+	}
+
+	// Button for skills
+	lv_obj_t *skills_btn = lv_list_add(r_list, NULL, "Skills", r_select_act);
+
+	// Register skills button
+	int btn_id = lv_obj_get_free_num(skills_btn);
+	btn_routine_ids.emplace(btn_id, -1);
+
+	// Done button
+	lv_obj_t *done = lv_btn_create(lv_scr_act(), NULL);
+	lv_obj_align(done, NULL, LV_ALIGN_IN_BOTTOM_LEFT, 0, 0);
+	lv_obj_t *done_label = lv_label_create(done, NULL);
+	lv_label_set_text(done_label, "Done");
+
+	// Wait for user to be done or for match to start
+	while (!user_is_done && pros::competition::is_disabled())
+		pros::delay(100);
+
+	// Return selected routine
+	return auton::routines.at(selected_r_id); // This shouldnt ever throw an exception, I hope...
 }
 
 // ================================ Game UI ================================ //
