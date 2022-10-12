ebed9a07814092e9e555fa63e5925e2993ebacb8
Thurston Yates <thurstonalexyates@gmail.com>
2022-10-12 15:52:23 -0400

    Add roller stuff

--- src/devices.cpp
+++ src/devices.cpp
@@ -1,9 +1,14 @@
 #include "devices.hpp"
+#include "pros/distance.hpp"
 
 // ================================ Devices ================================ //
 
 // Inputs
 okapi::Controller controller(okapi::ControllerId::master);
+
+// Sensors
+pros::Distance distance(9);
+pros::Optical optical(10);
 
 // Motors
 okapi::Motor drive_fl(1);
@@ -11,13 +16,18 @@ okapi::Motor drive_fr(2);
 okapi::Motor drive_rl(3);
 okapi::Motor drive_rr(4);
 
+pros::Motor roller(5);
+
 // Chassis
 std::shared_ptr<okapi::OdomChassisController> chassis =
     okapi::ChassisControllerBuilder()
         .withMotors(drive_fl, drive_fr, drive_rr, drive_rl)
         .withDimensions(
-            okapi::AbstractMotor::gearset::green,
-            {{4_in, 11.5_in}, okapi::imev5GreenTPR}
+            okapi::AbstractMotor::gearset::green, {{4_in, 11.5_in}, okapi::imev5GreenTPR}
         )
         .withOdometry()
         .buildOdometry();
+
+// ============================ Device functions ============================ //
+
+void do_roller() {}
