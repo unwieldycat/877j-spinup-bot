0a08be89c415d88ae862b06d8801a90f89dd08f4
Thurston Yates <thurstonalexyates@gmail.com>
2022-11-10 07:21:17 -0500

    Tidy up autonomous functions

--- src/auton.cpp
+++ src/auton.cpp
@@ -11,19 +11,26 @@ void auton::skills() {}
 /**
  * Win point routine
  */
-void solo_awp() {}
+void solo_awp() {
+	launcher.move(127);
+	auto_roller();
+	chassis->driveToPoint({-5.5_ft, -5.5_ft});
+	chassis->turnToAngle(135_deg);
+}
 
 /**
- * Only do closest roller and then move out way
+ * Only push discs into low goal
  */
 void preload() { chassis->driveToPoint({2_ft, 0_ft}); }
 
+// TODO: Simple roller routines
+void one_roller_left() {}
 void one_roller_right() {}
 
 /**
- * Do both rollers and ignore discs
+ * Do both rollers and ignore discs (starting from left)
  */
-void both_rollers_right() {
+void both_rollers_left() {
 	auto_roller();
 	chassis->driveToPoint({-5_ft, -5_ft});
 	chassis->turnToAngle(-90_deg);
@@ -31,7 +38,10 @@ void both_rollers_right() {
 	auto_roller();
 }
 
-void both_rollers_left() {
+/**
+ * Do both rollers and ignore discs (starting from right)
+ */
+void both_rollers_right() {
 	chassis->driveToPoint({-1.5_ft, 0_ft});
 	auto_roller();
 	chassis->driveToPoint({5_ft, 5_ft});
@@ -40,21 +50,12 @@ void both_rollers_left() {
 	auto_roller();
 }
 
-void awp_left() {}
-
-void awp_right() {
-	launcher.move(127);
-	auto_roller();
-	chassis->driveToPoint({-5.5_ft, -5.5_ft});
-	chassis->turnToAngle(135_deg);
-}
-
 // ============================ Routine Metadata ============================ //
 
 std::vector<Routine> auton::routines{
     Routine(solo_awp, start_position_e::LEFT, "Solo Autonomous Win Point"),
-    Routine(one_roller_right, start_position_e::BOTH, "Set One Roller"),
+    Routine(preload, start_position_e::BOTH, "Push discs into low goal"),
+    Routine(one_roller_right, start_position_e::RIGHT, "Set One Roller"),
+    Routine(one_roller_left, start_position_e::LEFT, "Set One Roller"),
     Routine(both_rollers_right, start_position_e::LEFT, "Set Both Rollers"),
-    Routine(both_rollers_left, start_position_e::RIGHT, "Set Both Rollers"),
-    Routine(awp_left, start_position_e::RIGHT, "Set Both Rollers"),
-    Routine(awp_right, start_position_e::RIGHT, "Set Both Rollers")};
+    Routine(both_rollers_left, start_position_e::RIGHT, "Set Both Rollers")};
