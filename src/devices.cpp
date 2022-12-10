bbb3c3c6ab37493c028eb59a1b21a4b03784179d
Thurston Yates <thurstonalexyates@gmail.com>
2022-12-10 10:41:43 -0500

    Make expansion faster

--- src/devices.cpp
+++ src/devices.cpp
@@ -190,10 +190,10 @@ void expand_control() {
 			expansion.move(0);
 			active = false;
 		} else if (status == -1 && active == false) {
-			expansion.move(-64);
+			expansion.move(-96);
 			active = true;
 		} else if (status == 1 && active == false) {
-			expansion.move(64);
+			expansion.move(96);
 			active = true;
 		}
 	}
