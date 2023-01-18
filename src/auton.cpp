ab106b3b3ee27c0c3c2a1b8825568f440f2cbdd6
Thurston Yates <thurstonalexyates@gmail.com>
2023-01-18 12:39:42 -0500

    Remove old code

--- src/auton.cpp
+++ src/auton.cpp
@@ -4,33 +4,7 @@ using namespace auton;
 
 // ============================= Skills Routine ============================= //
 
-void auton::skills() {
-	/*intake.set_encoder_units(pros::motor_encoder_units_e::E_MOTOR_ENCODER_DEGREES);
-	pusher.set_encoder_units(pros::motor_encoder_units_e::E_MOTOR_ENCODER_DEGREES);
-	chassis->driveToPoint({1_ft, 0.5_ft});
-	intake.move_absolute(210, 100);
-	chassis->driveToPoint({1_ft, -1_ft});
-	chassis->turnAngle(-90_deg);
-	launcher.move(127);
-	chassis->driveToPoint({3_ft, -2.5_ft});
-	intake.move_relative(210, 100);
-	chassis->driveToPoint({3_ft, -2.25_ft});
-	chassis->turnToAngle(0_deg);
-	pusher.move_relative(360, 100);
-	pros::delay(2000);
-	pusher.move_relative(360, 100);
-	chassis->driveToPoint({-4_ft, -5_ft});
-	launcher.move(0);
-	chassis->driveToPoint({-7_ft, -10_ft});
-	chassis->turnToAngle(-180_deg);
-	chassis->driveToPoint({-7_ft, -11.25_ft});
-	intake.move_relative(210, 100);
-	chassis->driveToPoint({-8_ft, -9_ft});
-	chassis->turnToAngle(90_deg);
-	chassis->driveToPoint({-9.25_ft, -9_ft});
-	intake.move_relative(210, 100);
-	chassis->driveToPoint({-8.5_ft, -8_ft});*/
-}
+void auton::skills() {}
 
 // ============================= Match Routines ============================= //
 
@@ -41,33 +15,10 @@ void do_nothing() {}
 
 /**
  * Win point routine
- * TODO: Make work
  */
-void solo_awp_left() {
-	/*chassis->setState({0_ft, 0_ft, 0_deg});
-	intake.set_encoder_units(pros::motor_encoder_units_e::E_MOTOR_ENCODER_DEGREES);
-	pusher.set_encoder_units(pros::motor_encoder_units_e::E_MOTOR_ENCODER_DEGREES);
-	chassis->driveToPoint({100_ft, 0_ft});
-	pros::delay(2000);*/
-}
+void solo_awp_left() {}
 
-void solo_awp_right() {
-	/*intake.set_encoder_units(pros::motor_encoder_units_e::E_MOTOR_ENCODER_DEGREES);
-	pusher.set_encoder_units(pros::motor_encoder_units_e::E_MOTOR_ENCODER_DEGREES);
-	chassis->driveToPoint({-2_ft, 0.5_ft});
-	intake.move_relative(180, 100);
-	chassis->driveToPoint({4_ft, -4_ft});
-	launcher.move(127);
-	chassis->turnToAngle(135_deg);
-	pusher.move_relative(360, 100);
-	pros::delay(2000);
-	pusher.move_relative(360, 100);
-	chassis->driveToPoint({5_ft, -8.5_ft});
-	launcher.move(0);
-	chassis->turnToAngle(-90_deg);
-	chassis->driveToPoint({6.5_ft, 8.5_ft});
-	intake.move_relative(180, 100);*/
-}
+void solo_awp_right() {}
 
 /**
  * Only push discs into low goal
@@ -76,45 +27,19 @@ void preload() { /*chassis->moveDistance(-2_ft);*/
 }
 
 // TODO: Simple roller routines
-void one_roller_left() {
-	/*intake.set_encoder_units(pros::motor_encoder_units_e::E_MOTOR_ENCODER_DEGREES);
-	chassis->driveToPoint({0_ft, 0.5_ft});
-	intake.move_absolute(180, 100);*/
-}
-void one_roller_right() {
-	/*intake.set_encoder_units(pros::motor_encoder_units_e::E_MOTOR_ENCODER_DEGREES);
-	chassis->driveToPoint({-2_ft, 0.5_ft});
-	intake.move_absolute(180, 100);*/
-}
+void one_roller_left() {}
+
+void one_roller_right() {}
 
 /**
  * Do both rollers and ignore discs (starting from left)
  */
-void both_rollers_left() {
-	/*intake.set_encoder_units(pros::motor_encoder_units_e::E_MOTOR_ENCODER_DEGREES);
-	chassis->driveToPoint({1_ft, 0.5_ft});
-	intake.move_relative(180, 100);
-	chassis->driveToPoint({-4_ft, -4_ft});
-	chassis->turnToAngle(90_deg);
-	chassis->driveToPoint({-7_ft, -9_ft});
-	chassis->driveToPoint({-9.5_ft, -9_ft});
-	intake.move_relative(180, 100);
-	chassis->driveToPoint({-9_ft, -9_ft});*/
-}
+void both_rollers_left() {}
 
 /**
  * Do both rollers and ignore discs (starting from right)
  */
-void both_rollers_right() {
-	/*intake.set_encoder_units(pros::motor_encoder_units_e::E_MOTOR_ENCODER_DEGREES);
-	chassis->driveToPoint({-2_ft, 0.5_ft});
-	intake.move_relative(180, 100);
-	chassis->driveToPoint({4_ft, -4_ft});
-	chassis->driveToPoint({5_ft, -8.5_ft});
-	chassis->turnToAngle(-90_deg);
-	chassis->driveToPoint({6.5_ft, 8.5_ft});
-	intake.move_relative(180, 100);*/
-}
+void both_rollers_right() {}
 
 // ============================ Routine Metadata ============================ //
 
